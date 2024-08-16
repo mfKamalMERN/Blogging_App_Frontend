df0e9fb2480b17d528d0212bf582317bc33a64a4
mufaisa <mfkamalimdia@gmail.com>
2024-08-17 00:00:44 +0530

    Added Login Image.

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -97,96 +97,103 @@ const Login = () => {
     return (
         <div>
             <Navbar isSignup={isSignup} toggleForm={toggleForm} isLogin={true} />
+            <div className="all" style={{ display: 'flex', justifyContent: "space-evenly" }}>
 
-            <h1 style={{ color: "wheat", marginTop: "120px" }}>{Localization.navbar.brand}</h1>
 
-            <div className={styles.formContainer}>
+                <img src="https://files.oaiusercontent.com/file-Bb3xl4V6hgMwPwSX0B4ZFmj9?se=2024-08-16T18%3A28%3A43Z&sp=r&sv=2023-11-03&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3De39b3d1a-008d-4482-a205-586712ac1746.webp&sig=eIR5QwRRIvYg182GoFlj6rq31dmqxK%2BFHxnC8%2Bu8BtY%3D" alt="" />
 
-                <h2>{isSignup ? Localization.navbar.signup : Localization.navbar.login}</h2>
+                <div className="contents">
 
-                <form onSubmit={handleSubmit} className={styles.form}>
+                    <h1 style={{ color: "wheat", marginTop: "120px" }}>{Localization.navbar.brand}</h1>
 
-                    <div className={styles.formGroup}>
-                        <label>Email:</label>
-                        <input
-                            type="email"
-                            placeholder='Email...'
-                            value={formdata.email}
-                            onChange={handleChange}
-                            required
-                            className={styles.input}
-                            name='email'
-                        />
-                    </div>
+                    <div className={styles.formContainer}>
 
-                    <div className={styles.formGroup}>
-                        <label>Password:</label>
-                        <input
-                            type="password"
-                            placeholder='Password...'
-                            value={formdata.password}
-                            onChange={handleChange}
-                            required
-                            className={styles.input}
-                            name='password'
-                        />
-                    </div>
+                        <h2>{isSignup ? Localization.navbar.signup : Localization.navbar.login}</h2>
 
-                    {isSignup && (
-                        <>
+                        <form onSubmit={handleSubmit} className={styles.form}>
+
                             <div className={styles.formGroup}>
-                                <label>Confirm Password:</label>
+                                <label>Email:</label>
+                                <input
+                                    type="email"
+                                    placeholder='Email...'
+                                    value={formdata.email}
+                                    onChange={handleChange}
+                                    required
+                                    className={styles.input}
+                                    name='email'
+                                />
+                            </div>
+
+                            <div className={styles.formGroup}>
+                                <label>Password:</label>
                                 <input
                                     type="password"
-                                    placeholder='Retype Password...'
-                                    value={formdata.confirmPassword}
+                                    placeholder='Password...'
+                                    value={formdata.password}
                                     onChange={handleChange}
                                     required
                                     className={styles.input}
-                                    name='confirmPassword'
+                                    name='password'
                                 />
                             </div>
 
-                            <div className={styles.formGroup}>
-                                <label>Name:</label>
-                                <input
-                                    type="text"
-                                    placeholder='Name...'
-                                    value={formdata.name}
-                                    onChange={handleChange}
-                                    required
-                                    className={styles.input}
-                                    name='name'
-                                />
-                            </div>
+                            {isSignup && (
+                                <>
+                                    <div className={styles.formGroup}>
+                                        <label>Confirm Password:</label>
+                                        <input
+                                            type="password"
+                                            placeholder='Retype Password...'
+                                            value={formdata.confirmPassword}
+                                            onChange={handleChange}
+                                            required
+                                            className={styles.input}
+                                            name='confirmPassword'
+                                        />
+                                    </div>
 
-                            <div className={styles.formGroup}>
-                                <label>Contact:</label>
-                                <input
-                                    type="text"
-                                    placeholder='Contact...'
-                                    value={formdata.contact}
-                                    onChange={handleChange}
-                                    required
-                                    className={styles.input}
-                                    name='contact'
-                                />
-                            </div>
-                        </>
-                    )}
+                                    <div className={styles.formGroup}>
+                                        <label>Name:</label>
+                                        <input
+                                            type="text"
+                                            placeholder='Name...'
+                                            value={formdata.name}
+                                            onChange={handleChange}
+                                            required
+                                            className={styles.input}
+                                            name='name'
+                                        />
+                                    </div>
 
-                    <button type="submit" className={styles.button}>
-                        {isSignup ? Localization.navbar.signup : Localization.navbar.login}
-                    </button>
+                                    <div className={styles.formGroup}>
+                                        <label>Contact:</label>
+                                        <input
+                                            type="text"
+                                            placeholder='Contact...'
+                                            value={formdata.contact}
+                                            onChange={handleChange}
+                                            required
+                                            className={styles.input}
+                                            name='contact'
+                                        />
+                                    </div>
+                                </>
+                            )}
 
-                </form>
+                            <button type="submit" className={styles.button}>
+                                {isSignup ? Localization.navbar.signup : Localization.navbar.login}
+                            </button>
 
-                <button onClick={isSignup ? () => toggleForm(false) : () => toggleForm(true)} className={styles.toggleButton}>
-                    {isSignup ? Localization.signupPage.loginPrompt : Localization.loginPage.signupPrompt}
-                </button>
+                        </form>
 
+                        <button onClick={isSignup ? () => toggleForm(false) : () => toggleForm(true)} className={styles.toggleButton}>
+                            {isSignup ? Localization.signupPage.loginPrompt : Localization.loginPage.signupPrompt}
+                        </button>
+
+                    </div>
+                </div>
             </div>
-
         </div>
     );
 };
