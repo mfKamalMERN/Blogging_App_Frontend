23834bde1cfbce2422e9b81a5ee66cb9314fd765
mufaisa <mfkamalimdia@gmail.com>
2024-08-16 19:41:04 +0530

    Changes on Followings page.

--- src/Pages/Followings.jsx
+++ src/Pages/Followings.jsx
@@ -8,6 +8,7 @@ const Followings = () => {
     const nav = useNavigate()
     const [Followings, setFollowings] = useState([])
     const [IsFollowing, setIsFollowing] = useState(false)
+    const [fstatus, setFstatus] = useState(false)
     const [userName, setUserName] = useState("")
 
     const { userid } = useParams()
@@ -32,7 +33,7 @@ const Followings = () => {
 
     useEffect(() => {
         tokenChecker()
-    }, [Followings, IsFollowing])
+    }, [fstatus])
 
 
     const checkFollowingStatus = (usrid) => {
@@ -58,6 +59,8 @@ const Followings = () => {
         try {
             await axios.put(`http://localhost:7500/followunfollow/${usrid}`)
             checkFollowingStatus(usrid)
+            setFstatus(!fstatus)
+
 
         } catch (error) {
             console.log(error);
@@ -66,7 +69,7 @@ const Followings = () => {
     }
 
     const isLoggedUser = (usrid) => {
-        if (JSON.parse(localStorage.getItem('LoggedInUser'))._id == usrid) {
+        if (JSON.parse(localStorage.getItem('LoggedInUser'))._id === usrid) {
             return true
         }
         else return false
@@ -77,28 +80,30 @@ const Followings = () => {
         <div>
             <Navbar />
             <div className={styles.container}>
-                <h2>Followings by {getUserName(userid)}</h2>
+                {Followings.length === 0 ?
+                    <h1>{getUserName(userid)} doesn't follow anyone yet</h1>
+                    :
+
+                    <h2>Followings by {getUserName(userid)}</h2>
+                }
                 <div className={styles.list}>
-                    {Followings.map((following) => (
+                    {
+                        Followings.map((following) => (
 
-                        <div key={following._id} className={styles.following}>
+                            <div key={following._id} className={styles.following}>
 
-                            <div onClick={() => nav(`/profile/${following._id}`)} className="imgAndName" style={{ display: "flex", alignItems: "center", marginRight: "10px" }}>
-                                <img src={following.DP} alt={following.Name} className={styles.avatar} />
-                                <div>{following.Name}</div>
-                            </div>
-                            {
-                                isLoggedUser(following._id) ?
-                                    <></>
-                                    :
-                                    checkFollowingStatus(following._id) ?
-                                        <button onClick={() => FollowUnfollow(following._id)} className={styles.button}>Unfollow</button>
+                                <div onClick={() => nav(`/profile/${following._id}`)} className="imgAndName" style={{ display: "flex", alignItems: "center", marginRight: "10px" }}>
+                                    <img src={following.DP} alt={following.Name} className={styles.avatar} />
+                                    <div>{following.Name}</div>
+                                </div>
+                                {
+                                    isLoggedUser(following._id) ?
+                                        <></>
                                         :
-                                        <button onClick={() => FollowUnfollow(following._id)} className={styles.button}>Follow</button>
-
-                            }
-                        </div>
-                    ))}
+                                        <button onClick={() => FollowUnfollow(following?._id)} className={styles.button}>{checkFollowingStatus(following?._id) ? 'Unfollow' : 'Follow'}</button>
+                                }
+                            </div>
+                        ))}
                 </div>
             </div>
             <button onClick={() => nav(-1)} className={styles.button}>Back</button>
