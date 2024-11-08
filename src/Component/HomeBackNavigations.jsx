05970831667b28bb380ee301358e35b8742b5bf5
mufaisa <mfkamalimdia@gmail.com>
2024-11-08 22:50:06 +0530

    c

--- src/Component/HomeBackNavigations.jsx
+++ src/Component/HomeBackNavigations.jsx
@@ -5,7 +5,7 @@ export const HomeBackNavigations = (props) => {
     const nav = useNavigate();
 
     return (
-        <div style={{ display: "flex", justifyContent: "space-evenly", width: "100%", marginTop: `${(!F && !NF) ? '0px' : '60px'}` }}>
+        <div style={{ display: "flex", justifyContent: "space-evenly", width: "100%", marginTop: `${(!F && !NF) ? '0px' : '80px'}` }}>
 
             <button onClick={() => nav(-1)} className={styles.button} style={{ backgroundColor: "black", }}><h1>🔙</h1></button>
 
