1c01b8c3076db4a8271ac2fcd14f7fbbee7f24ad
mufaisa <mfkamalimdia@gmail.com>
2024-12-24 08:41:32 +0530

    email unsend feature added.

--- src/Pages/Emails.jsx
+++ src/Pages/Emails.jsx
@@ -5,6 +5,7 @@ import Cookies from "universal-cookie";
 import Navbar from "../Component/Navbar";
 import styles from '../Styles/Emails.module.css'; // Import the CSS file for styling
 import { useEmailClk } from "../Helpers/Functions";
+import { toast } from "react-toastify";
 
 export const Emails = () => {
     const [emails, setEmails] = useState([]);
@@ -55,6 +56,31 @@ export const Emails = () => {
         nav(`/emails/${loggeduserid}`)
     }
 
+    const UnsendMail = (emailId) => {
+
+        if (window.confirm(`Unsending mail..`)) {
+            axios.delete(`https://blogging-app-backend-dpk0.onrender.com/unsendemail/${emailId}/${loggeduserid}`)
+                .then(async (response) => {
+                    if (response.status === 200) {
+                        try {
+                            const { data } = await axios.get(`https://blogging-app-backend-dpk0.onrender.com/emailssent/${loggeduserid}`);
+                            toast('Email Unsent Successfully')
+                            setEmails(data);
+                            nav(-1)
+                            return;
+                        } catch (error) {
+                            console.error(error);
+                        }
+                        // window.location.reload()
+                        // nav(-1)
+                    }
+                    alert('Error Occurred')
+
+                })
+                .catch((error) => console.error(error))
+        }
+    }
+
 
     return (
         axiosError ? <div style={{ color: "wheat" }}>Something went wrong</div> :
@@ -71,6 +97,7 @@ export const Emails = () => {
                     <div className={styles.emailbar} key={email._id} onClick={() => EmailClick(email._id)}>
                         <h4>{!sentmails ? (`From: ${email?.SentBy}`) : (`To: ${email?.SentTo}`)}</h4>
                         {email.Subject && <h5>Subject: {email?.Subject}</h5>}
+                        {sentmails && <button onClick={() => UnsendMail(email._id)}>Unsend</button>}
                     </div>
                 ))}
             </>
